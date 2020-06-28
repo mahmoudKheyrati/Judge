5ed95bc68c2b960252b8b3e8037099baca607425
Saeed-Sky <sr.amanat80@gmail.com>
2020-06-28 16:09:19 +0430

    cli prototype is finished. Hope to be in the group purpose direction. cli will be modified later when new changes be pushed to git. I also hope I didn't make any small or BIG mistakes

--- cli/Cli.c
+++ cli/Cli.c
@@ -1,8 +1,9 @@
 #include "Cli.h"
 #include <windows.h>
 #define MAX_FILE_NAME 1000
+#define MAX_ITEM_MEMORY 20000
 
-enum keyNameFlags{Compile = 1, GenerateTestCases, JudgeShow, Help, History, Exit};
+enum keyNameFlags{Compile = 1, GenerateTestCases, JudgeShow, Help, History, End};
 
 /**
  * Changes cmd font color
@@ -20,19 +21,19 @@ void changeConsoleColor(int colorCode) {
  * @param resultData
  */
 void printResult(String resultData){
-    changeConsoleColor(COLOR_YELLOW);
-    print("The result of judging your code is:\n");
+    changeConsoleColor(COLOR_BLOCK_YELLOW);
+    print("The result of judging your code is: \n");
 
     changeConsoleColor(COLOR_LIGHT_BLUE);
-    print("%s", resultData);
+    print("%20s", resultData);
 }
 
 /**
- * if the desired dir is not available this function makes it
+ * if the desired dir is not available this function create it
  * @param path
  */
 void makeGuardedHistoryDir(char path[]){
-    String* name = malloc(1000 * sizeof(char));
+    String* name = malloc(MAX_FILE_NAME * sizeof(char));
 
     if (isFolderExist(path) == False) {
         name[0] = path;
@@ -50,7 +51,7 @@ void makeGuardedHistoryDir(char path[]){
  */
 int saveAndShowCurrentHistory(struct ResultData* data) {
     char choose;
-    String resultString = malloc(MAX_FILE_NAME * sizeof(char));
+    String resultString;
 
     resultString = toJson(data);
     printResult(resultString);
@@ -67,17 +68,17 @@ int saveAndShowCurrentHistory(struct ResultData* data) {
         }
     }
     if (choose == 'n') {
-        return Exit;
+        return End;
 
     } else {
-        String fileName = malloc(MAX_FILE_NAME * sizeof(char));;
+        String fileName = malloc(MAX_FILE_NAME * sizeof(char));
 
         makeGuardedHistoryDir("\\.cJudge_history");
-        makeGuardedHistoryDir("\\.cJudge_hidden");
+        makeGuardedHistoryDir("\\.cJudge_hidden_history");
 
         sprintf(fileName,"%s_%s", data->fileName, data->date);
         writeFile("\\.cJudge_history" , fileName, resultString);
-        writeFile("\\.cJudge_hidden" ,fileName, resultString);
+        writeFile("\\.cJudge_hidden_history" ,fileName, resultString);
 
         free(fileName);
     }
@@ -86,77 +87,153 @@ int saveAndShowCurrentHistory(struct ResultData* data) {
 }
 
 /**
- * As you enter your commands in cmd to cJudge, this function detect your arguments and track your arguments
+ * this function shows you the name of all items in the hidden history directory.
+ */
+void showHistoryList(){
+    int* historyItemsNumber = malloc(MAX_ITEM_MEMORY * sizeof(int));
+    String* historyList = getFilesInDirectory("\\.cJudge_hidden_history", historyItemsNumber);
+
+    changeConsoleColor(COLOR_BLOCK_YELLOW);
+    print("Your total number of history files is '%d'; Your file names is as follows:\n\n", *historyItemsNumber);
+    changeConsoleColor(COLOR_WHITE);
+
+    loop(i, *historyItemsNumber){
+        print(" %d.  %s\n", i, historyList[i]);
+    }
+
+
+
+    free(historyItemsNumber);
+    free(historyList);
+}
+
+/**
+ * as you can guess this function shows all files available in the hidden directory that contains your results history
+ * @return enum that shows if the function works successfully
+ */
+int showHistoryFile(){
+    String historyFileName = malloc(MAX_FILE_NAME * sizeof(char));
+
+    print("Enter your file name (you can see the list of your history files, just enter 'cjudge -help'): ");
+    scanf("%s", historyFileName);
+    if (readFile("\\.cJudge_hidden_history", historyFileName) != NULL) {
+        system("pause");
+        return True;
+    }
+    print(" please enter your result file name correctly\n");
+    return False;
+}
+
+/**
+ * this function is menu that contains two options. based on user argument it acts differently
+ * @param userArgument  the argument user has entered
+ * @return enum that shows if the function works successfully
+ */
+int historyMenu(String userArgument){
+    if (strcmp(userArgument, "showlist") == 0){
+        showHistoryList();
+        return True;
+
+    } else if(strcmp(userArgument, "show") == 0){
+        if(showHistoryFile() == True)
+            return True;
+        else
+            return False;
+
+    } else{
+        changeConsoleColor(COLOR_RED);
+        print("invalid argument entered ");
+        return False;
+    }
+}
+
+/**
+ * As you enter your commands in cmd to cJudge, this function detect your arguments and track your sub-arguments
  * @param keyArgument your main (key) argument that causes the program do sth for you
  * @param userArgument follow your argument number by  incrementing in respect of your main argument parameters
  * @return enum shows if your program worked successfully
  */
-int keyDetermine(String keyArgument, int* userArgument){
-    if (strcmp(keyArgument, "compile") != 0) {
+int keyDetect(String keyArgument, int* userArgument){
+
+    if (strcmp(keyArgument, "compile") == 0) {
         *userArgument += 3;
         return Compile;
 
-    } else if (strcmp(keyArgument, "generate") != 0) {
+    } else if (strcmp(keyArgument, "generate") == 0) {
         *userArgument += 4;
         return GenerateTestCases;
 
-    } else if (strcmp(keyArgument, "judge") != 0){
+    } else if (strcmp(keyArgument, "judge") == 0){
         *userArgument += 3;
         return JudgeShow;
 
-    } else if (strcmp(keyArgument, "-help") != 0){
-        *userArgument += 1;
+    } else if (strcmp(keyArgument, "-help") == 0){
+        *userArgument += 0;
         return Help;
 
-    } else if(strcmp(keyArgument, "exit") != 0){
+    } else if (strcmp(keyArgument, "history") == 0){
+        *userArgument += 1;
+        return History;
 
-        return Exit;
+    } else if(strcmp(keyArgument, "end") == 0){
+
+        return End;
     }
     return False;
 }
 
-
-
-
-int keyCommandExecute(int keyCommandFlag,String arguments[],const int userArgument){
+/**
+ * this function executes the main arguments the user enters; Just like a menu
+ * @param keyCommandFlag  determines what this function should do
+ * @param arguments  user arguments that has been entered
+ * @param userArgument  number of expected user arguments
+ * @return enum that shows if there is any problem
+ */
+int keyCommandMenu(int keyCommandFlag, String *arguments, const int userArgument){
     struct ResultData *data;
     int validation;
 
     switch (keyCommandFlag) {
         case Compile:
-            compile(arguments[userArgument - 2], arguments[userArgument - 1], arguments[userArgument]);
-            return Exit;
+            compile(arguments[userArgument - 1], arguments[userArgument - 2], arguments[userArgument]);
+            return True;
 
         case GenerateTestCases:
-            generate(arguments[userArgument - 3], arguments[userArgument - 2],
+            generate(arguments[userArgument - 2], arguments[userArgument - 3],
                     arguments[userArgument - 1], arguments[userArgument]);
-            return Exit;
+            return True;
 
         case JudgeShow:
-            data = judgeAll(arguments[userArgument - 3], arguments[userArgument - 2],
-                            arguments[userArgument - 1], arguments[userArgument]);
+            data = judgeAll(arguments[userArgument - 1], arguments[userArgument],
+                            arguments[userArgument - 2], arguments[userArgument - 3]);
             if (data == NULL)
                 return False;
 
             validation = saveAndShowCurrentHistory(data);
             return validation;
 
+        case History:
+            historyMenu(arguments[userArgument]);
 
         case Help:
+            helpDescription();
+            return True;
 
-            // a function in header
-            break;
-
-        case Exit:
-
-            return Exit;
+        case End:
+            return End;
 
         default:
-
+            changeConsoleColor(COLOR_BLOCK_RED);
+            print("the argument you entered is invalid\n");
             return False;
     }
 }
-
+/**
+ * main function causes executing cli
+ * @param argc
+ * @param argv
+ * @return
+ */
 int runCli(int argc, String argv[]){
     int keyCommandFlag;
     int keyExecuteResult;
@@ -166,24 +243,28 @@ int runCli(int argc, String argv[]){
     print("if you are new to this program, you can enter \"cjudge -help\"\n");
 
     while(*userInput < argc){
-        if(*userInput != 0)
+
             *userInput += 1;
 
-        keyCommandFlag = keyDetermine(argv[*userInput], userInput);
-        keyExecuteResult = keyCommandExecute(keyCommandFlag, argv, *userInput);
+        keyCommandFlag = keyDetect(argv[*userInput], userInput);
+        keyExecuteResult = keyCommandMenu(keyCommandFlag, argv, *userInput);
 
         if (keyExecuteResult == False) {
             changeConsoleColor(COLOR_BLOCK_RED);
             print(" ...The program stops here\n");
             return False;
 
-        } else if (keyExecuteResult == Exit) {
+        } else if (keyExecuteResult == End) {
             changeConsoleColor(COLOR_BLOCK_GREEN);
             print("Have a nice time!\n");
+            changeConsoleColor(COLOR_WHITE);
 
             return True;
         }
     }
+    changeConsoleColor(COLOR_BLOCK_GREEN);
+    print("Thanks for using cjudge!\n");
+    changeConsoleColor(COLOR_WHITE);
 
     return True;
 }
--- cli/Cli.h
+++ cli/Cli.h
@@ -15,36 +15,98 @@
 
 void changeConsoleColor(int colorCode);
 
+int runCli(int argc, String argv[]);
+
+/**
+ * tutorial for user
+ */
 void helpDescription(){
     changeConsoleColor(COLOR_WHITE);
     print("This is a multipurpose judge program which will be introduced to you and how to use it:\n\n");
 
-    print("1. compile:\n");
+    print("1. compile:\n\n");
     changeConsoleColor(COLOR_BLOCK_GREEN);
-    print("Usage:");
+    print("  Usage:");
     changeConsoleColor(COLOR_WHITE);
-    print("%20s\n", "It compiles your code and make an exe file. You can compile either c program or java program");
+    print("%20s\n\n", "It compiles your code and make an exe file. You can compile either c program or java program.");
     changeConsoleColor(COLOR_LIGHT_BLUE);
-    print("How to call it:");
+    print("  How to call it:");
     changeConsoleColor(COLOR_WHITE);
-    print("%20s\n", "cjudge compile yourCodePath yourCodeName.c compiledFileName.exe");
-    print("%20s\n", "Enter your code path carefully. Then enter the name of your code file with the exact extension of c");
-    print("%20s\n\n", "or java source code. At last, enter the desired name for the compiled exe file");
+    print("%20s\n", "cjudge compile your_code_name.c your_code_path compiled_file_name.exe");
+    print("%20s\n", "Enter the name of your code file with the exact extension of c or java source code. Then enter");
+    print("%20s\n", "your code path carefully, either absolute or relative address, like: ");
+    print("%20s\n", ".\\my_first_folder\\my_second_folder\\in (your input folder)");
+    print("%20s\n\n\n", "At last, enter the desired name for the compiled exe or jar file with its extension");
 
-    print("1. generate the testcases:\n");
+
+    print("2. generate the test cases:\n\n");
     changeConsoleColor(COLOR_BLOCK_GREEN);
-    print("Usage:");
+    print("  Usage:");
     changeConsoleColor(COLOR_WHITE);
-    print("%20s\n", "");
+    print("%20s\n\n", "It generates test cases by reading your inputs and making proper output.");
     changeConsoleColor(COLOR_LIGHT_BLUE);
-    print("How to call it:");
+    print("  How to call it:");
     changeConsoleColor(COLOR_WHITE);
-    print("%20s\n", "cjudge compile yourCodePath yourCodeName.c compiledFileName.exe");
-    print("%20s\n", "Enter your code path carefully. Then enter the name of your code file with the exact extension of c");
-    print("%20s\n\n", "or java source code. At last, enter the desired name for the compiled exe file");
+    print("%20s\n", "cjudge generate your_code_name.c your_code_path your_inputs(folder)_path the_output(folder)_path");
+    print("%20s\n", "Enter the name of your code file as said in compile_section. Then enter your code path. And then");
+    print("%20s\n", "enter the paths of your input directory and the output path that generated outputs will be placed");
+    print("%20s\n\n\n", "in there. Enter your paths as said before ↑");
+
+    print("3. judge your code:\n\n");
+    changeConsoleColor(COLOR_BLOCK_GREEN);
+    print("  Usage:");
+
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n", "Here is the main work that this program does for you. Judging your program means giving the");
+    print("%20s\n", "judge your outputs. Then... judge tells you your based on your outputs and comparing them with");
+    print("%20s\n", "GENERATED outputs. So, please take note that you should 'generate test cases' before judging. it");
+    print("%20s\n\n", "also asks you whether you want to save history after showing the result of judge.");
+
+    changeConsoleColor(COLOR_LIGHT_BLUE);
+    print("  How to call it:");
+
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n", "cjudge judge your_code_name.c your_code_path your_output_path generated(answer)_output_path");
+    print("%20s\n", "Enter your code name with c or java extension. Then enter your file path. And then enter the paths ");
+    print("%20s\n", "of your output directory and the generated output directory. Enter your paths as said before");
+    print("%20s\n\n\n", "Please take note that generated output directory is important to contain valid & true answers!");
 
 
+    print("4. see the history files:\n\n");
+    changeConsoleColor(COLOR_BLOCK_GREEN);
+    print("  Usage:");
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n", "You can see the list of your result files that are saved automatically. You can also see what these ");
+    print("%20s\n", "result files contain.");
 
+    changeConsoleColor(COLOR_LIGHT_BLUE);
+    print("  How to call it:\n");
+    changeConsoleColor(COLOR_WHITE);
+
+    changeConsoleColor(COLOR_YELLOW);
+    print("  list of result files:");
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n", " cjudge history showlist");
+
+    changeConsoleColor(COLOR_YELLOW);
+    print("  see the result:");
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n", " cjudge history show history_file_name");
+    print("%20s\n\n\n", "you can find your 'history file name' by calling \" history showlist\" as mentioned");
+
+
+    print("5. exit:\n\n");
+    changeConsoleColor(COLOR_BLOCK_GREEN);
+    print("  Usage:");
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n\n", "You can exit the program if you want to.");
+    changeConsoleColor(COLOR_LIGHT_BLUE);
+    print("  How to call it:");
+    changeConsoleColor(COLOR_WHITE);
+    print("%20s\n\n\n", "cjudge end");
+
+    changeConsoleColor(COLOR_BLOCK_GREEN);
+    print("this is the end of tutorial; Now, you are ready to use this program. call -help any time you need to. bye!\n");
     changeConsoleColor(COLOR_WHITE);
 
 }
