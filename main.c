b21910fa0efe64b8ebbb7aff22291f076ee3eae3
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2020-07-02 20:22:43 +0430

    release 0.0.1

--- main.c
+++ main.c
@@ -8,28 +8,6 @@
 #include "core/CompileHelper.h"
 
 int main(int argc, String argv[]) {
-    struct ResultData* data2 = judgeAll("./in","./out","./","aa.c",800);
-    print("%s",toJson(data2));
-//    struct TestCaseData case1 = {
-//
-//    };
-//    struct TestCaseData cases[3] = {{1, True, "a message", "output"},
-//                                    {2, False, "a something else", "output 2"},
-//                                    {3, True, "a message", "output 3"},
-//
-//    };
-//    struct ResultData data = {
-//            .path="path/to/file",
-//            .fileName="filename.c",
-//            .date="12/12/1212 12:12:12 gmt",
-//            .message="successful",
-//            .score="12/20",
-//            .testCaseNumber=3,
-//            .testCases=cases
-//    };
-//    String jsonData= toJson(&data);
-//    print("%s",jsonData);
-//    free(jsonData);
-//    runCli(argc,argv);
-     return 0;
+    runCli(argc, argv);
+    return 0;
 }
