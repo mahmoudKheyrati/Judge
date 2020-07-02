9056d7f79a65da884933a081f8b6833db5c354a3
mahmoud <mahmoud.kheyrati.fard@gmail.com>
2020-07-02 19:58:39 +0430

    to json improvment

--- core/Core.c
+++ core/Core.c
@@ -31,18 +31,18 @@ String toJson(struct ResultData *data) {
     String testCases = malloc(data->testCaseNumber * sizeof(struct TestCaseData));
 
     for (int i = 0; i < data->testCaseNumber; ++i) {
-        struct TestCaseData *testCase = data->testCases;
-        String item = malloc(MAX_SIZE_ITEM_STRING * sizeof(struct TestCaseData));
+        struct TestCaseData testCase = data->testCases[i];
+        String item = (String)malloc(MAX_SIZE_ITEM_STRING * sizeof(struct TestCaseData));
         print("");
         sprintf(item, "    {\n"
                       "      \t\"testCaseNumber\": %i,\n"
                       "      \t\"isPass\": %s,\n"
                       "      \t\"message\": \"%s\",\n"
-                      "      \t\"output\": \"%s\"\n"
-                      "    },\n", testCase->testCaseNumber, testCase->isPass ? "true" : "false",
-                testCase->message,
-                testCase->output);
-        strcat(testCases, item);
+                      "    },\n", testCase.testCaseNumber, testCase.isPass ? "true" : "false",
+                testCase.message);
+        String a[2]={testCases,item};
+        testCases= strConcat(a,2);
+//        strcat(testCases, item);
         free(item);
     }
     String result = malloc(MAX_SIZE_RESULT_STRING + MAX_SIZE_ITEM_STRING * data->testCaseNumber * sizeof(struct TestCaseData));
